62f1518e9b92bccc1ee305fc4e2f1164f9264d7f
WBomfim <willianbomfim@hotmail.com>
2022-08-16 17:39:26 -0300

    feat: Criado a função na camada model para buscar os dados de todas as vendas realizadas.

--- models/sales.js
+++ models/sales.js
@@ -1,5 +1,18 @@
 const connection = require('./connection');
 
+const getAllSales = async () => {
+  const query = (
+    `SELECT sl.id AS saleId, sl.date,
+    sp.product_id AS productId, sp.quantity
+    FROM StoreManager.sales AS sl
+    JOIN StoreManager.sales_products AS sp
+    ON sl.id = sp.sale_id;`
+  );
+  const [sales] = await connection.execute(query);
+  if (!sales) return null;
+  return sales;
+};
+
 const addSale = async () => {
   const query = 'INSERT INTO StoreManager.sales (date) VALUES (NOW());';
   const [{ insertId }] = await connection.execute(query);
@@ -15,6 +28,7 @@ const addSaleInfo = async (saleId, productId, quantity) => {
 };
 
 module.exports = {
+  getAllSales,
   addSale,
   addSaleInfo,
 };
