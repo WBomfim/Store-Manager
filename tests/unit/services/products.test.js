6abffdda9a61597af11a0de5e4bc112bdef5a68f
WBomfim <willianbomfim@hotmail.com>
2022-08-17 16:13:35 -0300

    test: Criado testes para cobrir a função que deleta um produto da camada services

--- tests/unit/services/products.test.js
+++ tests/unit/services/products.test.js
@@ -389,3 +389,82 @@ describe('Services - Ao atualizar um produto', () => {
     });
   });
 });
+
+describe('Quando um produto é excluído', () => {
+  beforeEach(sinon.restore);
+  const ID_TEST = 5;
+
+  describe('Quando o produto não está cadastrado no banco de dados', () => {
+    it('Deve retornar um objeto com as chaves code e error', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(null);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result).to.be.an('object');
+      expect(result).to.have.keys('code', 'error');
+    });
+
+    it('A chave code deve conter o código 404', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(null);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.code).to.be.equal(404);
+    });
+
+    it('A chave error deve conter a mensagem "Product not found"', async () => {
+      const ERROR_MESSAGE = 'Product not found';
+      sinon.stub(productsModel, 'getProductById').resolves(null);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.error).to.be.equal(ERROR_MESSAGE);
+    });
+  });
+
+  describe('Quando o produto não é excluído do banco de dados por algum motivo', () => {
+    const RETURN_PRODUCT_TEST = { id: ID_TEST, name: 'Martelo do Thor' };
+    it('Deve retornar um objeto com as chaves code e error', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves();
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result).to.be.an('object');
+      expect(result).to.have.keys('code', 'error');
+    });
+
+    it('A chave code deve conter o código 501', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves();
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.code).to.be.equal(501);
+    });
+
+    it('A chave error deve conter a mensagem "Product not deleted"', async () => {
+      const ERROR_MESSAGE = 'Product not deleted';
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves();
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.error).to.be.equal(ERROR_MESSAGE);
+    });
+  });
+
+  describe('Quando o produto é excluído do banco de dados', () => {
+    const RETURN_PRODUCT_TEST = { id: ID_TEST, name: 'Martelo do Thor' };
+
+    it('Deve retornar um objeto com as chaves code e data', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves(true);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result).to.be.an('object');
+      expect(result).to.have.keys('code', 'data');
+    });
+
+    it('A chave code deve conter o código 204', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves(true);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.code).to.be.equal(204);
+    });
+
+    it('A chave data deve conter um objeto vazio', async () => {
+      sinon.stub(productsModel, 'getProductById').resolves(RETURN_PRODUCT_TEST);
+      sinon.stub(productsModel, 'deleteProduct').resolves(true);
+      const result = await productsService.deleteProduct(ID_TEST);
+      expect(result.data).to.be.deep.equal({});
+    });
+  });
+});
