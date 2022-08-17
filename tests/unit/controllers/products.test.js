4afc90999c7a9dc5cc19b0f5ec45a75f801c8f8b
WBomfim <willianbomfim@hotmail.com>
2022-08-17 01:08:16 -0300

    test: Criado testes para cobrir o controller updateProduct

--- tests/unit/controllers/products.test.js
+++ tests/unit/controllers/products.test.js
@@ -259,3 +259,186 @@ describe('Controllers - Quando chamar o controller addProduct', () => {
     });
   });
 });
+
+describe('Controllers - Quando chamar o controller updateProduct', () => {
+  const req = {};
+  const res = {};
+
+  describe('Não inviando o nome do produto na requisição', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = {};
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_ERROR = {
+        code: 400,
+        error: '"name" is required'
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_ERROR);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 400', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com a menssagem de erro "name" is required"', async () => {
+      const OBJECT_RES = {
+        message: '"name" is required'
+      };
+      await productsController.updateProduct(req, res);
+      expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+
+  describe('Com o nome do produto não sendo uma palavra', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = { name: 12345 };
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_ERROR = {
+        code: 400,
+        error: '"name" must be a string"'
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_ERROR);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 400', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com a menssagem de erro "name" must only contain alpha-numeric characters"',
+      async () => {
+        const OBJECT_RES = {
+          message: '"name" must be a string"'
+        };
+        await productsController.updateProduct(req, res);
+        expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+
+  describe('Com o nome do produto tendo menos de 5 caracteres', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = { name: 'Thor ' };
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_ERROR = {
+        code: 422,
+        error: '"name" length must be at least 5 characters long'
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_ERROR);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 422', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com a menssagem de erro "name" length must be at least 5 characters long"',
+      async () => {
+        const OBJECT_RES = {
+          message: '"name" length must be at least 5 characters long'
+        };
+        await productsController.updateProduct(req, res);
+        expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+
+  describe('E o produto não é encontrado no banco de dados', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = { name: 'Martelo de Thor' };
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_ERROR = {
+        code: 404,
+        error: 'Product not found'
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_ERROR);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 404', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(404)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com a menssagem de erro "Product not found"', async () => {
+      const OBJECT_RES = {
+        message: 'Product not found'
+      };
+      await productsController.updateProduct(req, res);
+      expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+
+  describe('E o produto não é atualizado no banco de dados', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = { name: 'Martelo de Thor' };
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_ERROR = {
+        code: 501,
+        error: 'Product not updated'
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_ERROR);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 501', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(501)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com a menssagem de erro "Product not updated"', async () => {
+      const OBJECT_RES = { message: 'Product not updated' };
+      await productsController.updateProduct(req, res);
+      expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+
+  describe('E o produto é atualizado no banco de dados', () => {
+    before(() => {
+      req.params = { id: 1 };
+      req.body = { name: 'Martelo do Batman' };
+      res.status = sinon.stub().returnsThis();
+      res.json = sinon.stub().returns();
+      const OBJECT_SUCCESS = {
+        code: 200,
+        data: {
+          id: 1,
+          name: 'Martelo do Batman',
+        }
+      };
+      sinon.stub(productsService, 'updateProduct').resolves(OBJECT_SUCCESS);
+    });
+
+    after(() => productsService.updateProduct.restore());
+
+    it('Deve retornar o status com o código 200', async () => {
+      await productsController.updateProduct(req, res);
+      expect(res.status.calledWith(200)).to.be.true;
+    });
+
+    it('Deve retornar o objeto com o produto atualizado', async () => {
+      const OBJECT_RES = {
+        id: 1,
+        name: 'Martelo do Batman',
+      };
+      await productsController.updateProduct(req, res);
+      expect(res.json.calledWith(OBJECT_RES)).to.be.true;
+    });
+  });
+});
